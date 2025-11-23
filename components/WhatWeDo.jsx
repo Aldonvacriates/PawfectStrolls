c129c75d61fc561b3b7adfebe62a96c4bcae1ca2
Aldo Herrera <aldonvacriates@gmail.com>
2025-11-23 10:09:57 -0700

    whatwedo

--- components/WhatWeDo.jsx
+++ components/WhatWeDo.jsx
@@ -7,76 +7,106 @@ const WhatWeDo = () => {
 
   return (
     <>
-    <section className="what-we-do-sec">
+      <section className="what-we-do-sec">
         <div className="what-we-do-tit-wraper">
-            <h3 className='what-we-do-main-tit'>What We <b>Do</b>.</h3>
+          <h3 className="what-we-do-main-tit">
+            What We <b>Do</b>.
+          </h3>
         </div>
         <div className="container">
-            <div className="row">
-                <div className="col-lg-4" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">
-                        <div className="what-we-do-cards"  >
-                        <div className="what-we-do-card-img">
-                            <a href="#">
-                            <img src={Img1} alt="" />
-                            </a>
-                        </div>
-                        <a href="#" className="what-we-do-card-tit">
-                        Pet Grooming
-                        </a>
-
-                        <p>Risus sed vulputate odio ut enim. Cursus in hac habitasse platea dictumst. Tortor dignissim convallis aenean et tortor. Etiam sit amet nisl purus in mollis nunc sed id.</p>
-
-                        <a href="#" className="abt-us-read-more-btn">
-                        Read more
-                        </a>
-
-                    </div>
+          <div className="row">
+            <div
+              className="col-lg-4"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              data-aos-delay="100"
+            >
+              <div className="what-we-do-cards">
+                <div className="what-we-do-card-img">
+                  <a href="#">
+                    <img src={Img1} alt="" />
+                  </a>
                 </div>
-                <div className="col-lg-4" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">
-                    <div className="what-we-do-cards"  >
-                        <div className="what-we-do-card-img animate__animated animate__bounce" >
-                            <a href="#">
-                            <img src={Img2} alt="" />
-                            </a>
-                        </div>
+                <a href="#" className="what-we-do-card-tit">
+                  Daily Walks & Exercise
+                </a>
 
-                        <a href="#" className="what-we-do-card-tit">
-                        Healthy Meals
-                        </a>
+                <p>
+                  We provide safe, structured walks tailored to your dog's
+                  energy level. Whether they need a quick potty break or a full
+                  adventure, our walks help reduce stress, burn energy, and keep
+                  your pup healthy and happy.
+                </p>
 
-                        <p>Risus sed vulputate odio ut enim. Cursus in hac habitasse platea dictumst. Tortor dignissim convallis aenean et tortor. Etiam sit amet nisl purus in mollis nunc sed id.</p>
-
-                        <a href="#" className="abt-us-read-more-btn">
-                        Read more
-                        </a>
-
-                    </div>
-                </div>
-                <div className="col-lg-4" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
-                    <div className="what-we-do-cards"  >
-                        <div className="what-we-do-card-img">
-                            <a href="#">
-                            <img src={Img3} alt="" />
-                            </a>
-                        </div>
-
-                        <a href="#" className="what-we-do-card-tit">
-                        Veterinary Service
-                        </a>
-
-                        <p>Risus sed vulputate odio ut enim. Cursus in hac habitasse platea dictumst. Tortor dignissim convallis aenean et tortor. Etiam sit amet nisl purus in mollis nunc sed id.</p>
-
-                        <a href="#" className="abt-us-read-more-btn">
-                        Read more
-                        </a>
-
-                    </div>
-                </div>
+                <a href="#" className="abt-us-read-more-btn">
+                  Read more
+                </a>
+              </div>
             </div>
+            <div
+              className="col-lg-4"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              data-aos-delay="300"
+            >
+              <div className="what-we-do-cards">
+                <div className="what-we-do-card-img animate__animated animate__bounce">
+                  <a href="#">
+                    <img src={Img2} alt="" />
+                  </a>
+                </div>
+
+                <a href="#" className="what-we-do-card-tit">
+                  Potty Breaks & Check-Ins
+                </a>
+
+                <p>
+                  Perfect for puppies, seniors, or busy days. We stop by to let
+                  your dog out, refresh water, give them attention, and make
+                  sure they're comfortable. A quick check-in that brings peace
+                  of mind and keeps your pet's routine consistent.
+                </p>
+
+                <a href="#" className="abt-us-read-more-btn">
+                  Read more
+                </a>
+              </div>
+            </div>
+            <div
+              className="col-lg-4"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              data-aos-delay="600"
+            >
+              <div className="what-we-do-cards">
+                <div className="what-we-do-card-img">
+                  <a href="#">
+                    <img src={Img3} alt="" />
+                  </a>
+                </div>
+
+                <a href="#" className="what-we-do-card-tit">
+                  In-Home Dog Sitting
+                </a>
+
+                <p>
+                  When you're away, we stay with your pup in the comfort of
+                  their own home. We keep their routine consistent with feeding,
+                  walks, playtime, and lots of affection. Your dog enjoys a
+                  stress-free environment while you get peace of mind knowing
+                  they're cared for like family.
+                </p>
+
+                <a href="#" className="abt-us-read-more-btn">
+                  Read more
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-    </section>
+      </section>
     </>
-  )
+  );
 }
 
 export default WhatWeDo
