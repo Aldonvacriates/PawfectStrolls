b3132018a647d8052f08a32e83ff2c4029739fad
Aldo Herrera <aldonvacriates@gmail.com>
2025-11-23 09:41:50 -0700

    get

--- components/GetEveryPetFoodsSec.jsx
+++ components/GetEveryPetFoodsSec.jsx
@@ -9,32 +9,45 @@ import VideoPause from "../src/assets/home/get-every-pet-foods/home-video-pusue.
 const GetEveryPetFoodsSec = () => {
   return (
     <>
-    <section className="get-every-pet-foods-sec">
+      <section className="get-every-pet-foods-sec">
         <div className="container">
-            <div className="row">
-                <div className="col-lg-6">
-                    <div className="get-every-pet-foods-cont">
-                    <h3 className="get-every-pet-foods-main-tit">
-                    <div>Get Every Pet </div><b>Foods</b> & <b>Toys Here.</b>
-                    </h3>
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="get-every-pet-foods-cont">
+                <h3 className="get-every-pet-foods-main-tit">
+                  <div>Get Every Pet </div>
+                  <b>Foods</b> & <b>Toys Here.</b>
+                </h3>
 
-                    <p>Risus sed vulputate odio ut enim. Cursus in hac habitasse platea dictumst. Tortor dignissim convallis aenean et tortor. Etiam sit amet nisl purus in mollis nunc sed id. Integer enim neque volutpat ac tincidunt vitae semper quis lectus.</p>
+                <p>
+                  Find everything your pets love in one place-from tasty treats
+                  to playful toys that keep them happy and active. We offer
+                  quality essentials designed for comfort, health, and everyday
+                  joy, making it easy to care for your furry companions with
+                  products you can trust and they'll adore.
+                </p>
 
-                    <a href="#" className="get-every-pet-foods-btn">
-                    Shop Now
-                    </a>
-                    </div>
-                </div>
-                <div className="col-lg-6">
-                <div className="get-every-pet-foods-img">
-                     <video id="video1" src={PetVideo} poster={Thumpnail} preload="auto" controls></video>
-                </div>
-                </div>
+                <a href="#" className="get-every-pet-foods-btn">
+                  Shop Now
+                </a>
+              </div>
             </div>
+            <div className="col-lg-6">
+              <div className="get-every-pet-foods-img">
+                <video
+                  id="video1"
+                  src={PetVideo}
+                  poster={Thumpnail}
+                  preload="auto"
+                  controls
+                ></video>
+              </div>
+            </div>
+          </div>
         </div>
-    </section>
+      </section>
     </>
-  )
+  );
 }
 
 export default GetEveryPetFoodsSec
