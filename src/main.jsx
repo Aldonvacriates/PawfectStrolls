12c44d1ef0ccc9796f2be69a1d5658bd4932d219
Aldo Herrera <aldonvacriates@gmail.com>
2025-12-04 20:15:32 -0700

    contacts

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { inject } from '@vercel/analytics'
 
 // import "../styles/swiper.css"
-
-// Initialize Vercel Web Analytics
-inject()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
